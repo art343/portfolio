de52d5b8690b99314bbb16214ccb0d7efe7d8b86
art343 <82078122+art343@users.noreply.github.com>
2025-09-16 11:29:56 -0700

    Add files via upload

---
+++ contact/contactthanks.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title><-- Favicon</title>
+  <link rel="shortcut icon" href="../links/logo.png" >
+  <link rel="shortcut icon" href="../links/logo.png" >
+  <meta name="author" content="guglielmopardo.me">
+  <meta name="description" content="########">
+  <meta name="keywords" content="########" />
+  <link rel="stylesheet" href="../css/foundation.css">
+  <link rel="stylesheet" href="../css/style.css">
+  <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	
+		<div id="wrapper">
+		<header><img src="links/logo.png" height="35" align="right" alt=""/></header>
+			<nav>
+<div class="topnav" id="myTopnav">
+  <a href="../index.html""" class="active" style="color: white;">Start over</a>
+  <a href="books.html">Books, interiors</a>
+  <a href="design.html">Print, web</a>
+  <a href="brand.html">Brand, advertising</a>	 	
+  <a href="photo.html"  class="active" style="color: white;">Photography</a>
+  <a href="contact.html">Say hello</a>		
+	
+<!--Begin Javascript -->		
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+<!--End Javascript -->	
+	</div><!--Closes the nav-->
+		</nav>
+<aside>
+<img src="../links/self_portrait.gif" width="200" alt=""/><br>    
+<p class="summaryhead" style="text-indent: -.065em;">Hey, thanks. I'll get back to you shortly, <br> but your messange hs been successfully sent.<br> Thank you.</p><br>
+	
+ </aside>
+		<footer><p style="">&nbsp</p>Built with HTML, CSS, and a dash of Javascript;<br>Works and concepts completed 2007-2021</footer>
+		<script>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+		</script>	
+  <script>
+  document.write('<script src=' +
+  ('__proto__' in {} ? '../js/vendor/zepto' : '../js/vendor/jquery') +
+  '.js><\/script>')
+  </script>
+  
+  <script src="../js/foundation.min.js"></script>
+  
+  <!--
+  <script src="js/foundation/foundation.js"></script>
+  <script src="js/foundation/foundation.alerts.js"></script>
+  <script src="js/foundation/foundation.clearing.js"></script>
+  <script src="js/foundation/foundation.orbit.js"></script>
+  -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+  <script>
+    $(document).foundation();
+  </script>
+  <script>
+    window.onload = function() {
+        setTimeout(function(){window.scrollTo(0, 1);}, 1);
+    }
+  </script>
+</body>
+</html>
